41a1941dbc9f573d4a85aea25e208a130a1cd9a0
Your Name <951437256@.com>
2020-06-16 19:33:02 +0800

    first commit

---
+++ com/cn/entity/Student.java
@@ -0,0 +1,11 @@
+package com.cn.entity;
+
+import sun.plugin.net.protocol.jar.CachedJarURLConnection;
+
+public class Student {
+    private  Integer age;
+    private String  name;
+    private Character sex;
+
+
+}
